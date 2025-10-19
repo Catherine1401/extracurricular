762f1b8ef8207e3758858c62aa6f9ff894201413
unknown <huyvu.hw@gmail.com>
2025-10-19 11:19:23 +0700

    feat/role-permission: admin: crud activities; user: read activity

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Notifications\Action;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,7 +17,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    require __DIR__.'/activities.php';
+
+    Route::middleware('check.role:admin')->group(function () {
+        Route::get('/activities/create', [ActivityController::class, 'create'])->name('activities.create');
+    });
+
+    Route::get('/activities', [ActivityController::class, 'index'])->name('activities.index');
+    Route::get('/activities/{activity}', [ActivityController::class, 'show'])->name('activities.show');
+
+
+
+});
+
+Route::middleware(['auth', 'check.role:admin'])->group(function () {
+    Route::post('/activities', [ActivityController::class, 'store'])->name('activities.store');
+    Route::get('/activities/{activity}/edit', [ActivityController::class, 'edit'])->name('activities.edit');
+    Route::put('/activities/{activity}', [ActivityController::class, 'update'])->name('activities.update');
+    Route::delete('/activities/{activity}', [ActivityController::class, 'destroy'])->name('activities.destroy');
 });
 
 require __DIR__.'/auth.php';
